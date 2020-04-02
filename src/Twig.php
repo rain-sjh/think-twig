d0155456bf0436e4d622b084c69d55a2c23a18ea
雨 <625250353@qq.com>
2020-04-02 17:50:37 +0800

    Update Twig.php

--- src/Twig.php
+++ src/Twig.php
@@ -1,13 +1,25 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
+
+/** @noinspection
+ * PhpUndefinedNamespaceInspection,
+ * PhpUndefinedClassInspection,
+ * SpellCheckingInspection,
+ * PhpUndefinedMethodInspection,
+ * ShortListSyntaxCanBeUsedInspection,
+ * PhpUnusedParameterInspection,
+ * PhpUndefinedFieldInspection,
+ * ReturnTypeCanBeDeclaredInspection,
+ * PhpUnused
+ */
+
+/**
+ * 工具: PhpStorm
+ * 作者: 孙家浩
+ * 邮箱: yulian1024@vip.qq.com
+ * 日期: 2020/3/25
+ * 时间: 20:01
+ * 侵权必究
+ */
 
 namespace think\view\driver;
 
@@ -15,6 +27,15 @@ use think\App;
 use think\exception\TemplateNotFoundException;
 use think\Loader;
 use think\Template;
+use think\Exception;
+use think\helper\Str;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+use Twig\TwigFunction;
+use Twig_Error_Loader;
+use Twig_Error_Runtime;
+use Twig_Error_Syntax;
+use TwigExpand;
 
 class Twig
 {
@@ -23,17 +44,17 @@ class Twig
 	// 模板引擎参数
 	protected $config = [
 		// 默认模板渲染规则 1 解析为小写+下划线 2 全部转换小写
-		'auto_rule' => 1,
+		'auto_rule'   => 1,
 		// 视图基础目录（集中式）
-		'view_base' => '',
+		'view_base'   => '',
 		// 模板起始路径
-		'view_path' => '',
+		'view_path'   => '',
 		// 模板文件后缀
 		'view_suffix' => 'html.twig',
 		// 模板文件名分隔符
-		'view_depr' => DIRECTORY_SEPARATOR,
+		'view_depr'   => DIRECTORY_SEPARATOR,
 		// 是否开启模板编译缓存,设为false则每次都会重新编译
-		'tpl_cache' => true,
+		'tpl_cache'   => true,
 	];
 
 	public function __construct(App $app, $config = [])
@@ -55,12 +76,12 @@ class Twig
 	/**
 	 * 检测是否存在模板文件
 	 * @access public
-	 * @param  string $template 模板文件或者模板规则
+	 * @param string $template 模板文件或者模板规则
 	 * @return bool
 	 */
 	public function exists($template)
 	{
-		if ('' == pathinfo($template, PATHINFO_EXTENSION)) {
+		if ('' === pathinfo($template, PATHINFO_EXTENSION)) {
 			// 获取模板文件名
 			$template = $this->parseTemplate($template);
 		}
@@ -71,17 +92,17 @@ class Twig
 	/**
 	 * 渲染模板文件
 	 * @access public
-	 * @param  string $template 模板文件
-	 * @param  array $data 模板变量
-	 * @param  array $config 模板参数
+	 * @param string $template 模板文件
+	 * @param array $data 模板变量
+	 * @param array $config 模板参数
 	 * @return void
-	 * @throws \Twig_Error_Loader
-	 * @throws \Twig_Error_Runtime
-	 * @throws \Twig_Error_Syntax
+	 * @throws Twig_Error_Loader
+	 * @throws Twig_Error_Runtime
+	 * @throws Twig_Error_Syntax
 	 */
 	public function fetch($template, $data = [], $config = [])
 	{
-		if ('' == pathinfo($template, PATHINFO_EXTENSION)) {
+		if ('' === pathinfo($template, PATHINFO_EXTENSION)) {
 			// 获取模板文件名
 			$template = $this->parseTemplate($template);
 		}
@@ -91,46 +112,46 @@ class Twig
 			throw new TemplateNotFoundException('template not exists:' . $template, $template);
 		}
 
+		$this->twigEngine($template, $data);
+	}
+
+	private function twigEngine($template, $data): void
+	{
 		// 记录视图信息
-		$this->app
-			->log('[ VIEW ] ' . $template . ' [ ' . var_export(array_keys($data), true) . ' ]');
+		$this->app['log']->record('[ VIEW ] ' . $template . ' [ ' . var_export(array_keys($data), true) . ' ]');
 
-		$path = [
-			$this->config['view_path'],
-			$this->config['view_path'] . request()->controller() . DIRECTORY_SEPARATOR
-		];
+		$loader = new FilesystemLoader([dirname($template), $this->config['view_path']]);
 
-		$loader = new \Twig\Loader\FilesystemLoader($path);
-		$twig = new \Twig\Environment($loader, [
+		$twig = new Environment($loader, [
 			'cache' => $this->config['tpl_cache'] ? $this->config['cache_path'] : false,
 		]);
 
 		//自定义模板函数
-		if(class_exists('\TwigExpand')){
-			$twig->addExtension(new \TwigExpand());
+		if (class_exists('\TwigExpand')) {
+			$twig->addExtension(new TwigExpand());
 		}
 
 		//函数动态定义
-		$twig->registerUndefinedFunctionCallback(function ($name) {
+		$twig->registerUndefinedFunctionCallback(static function ($name) {
 			if (function_exists($name)) {
-				return new \Twig\TwigFunction($name, $name, ['is_safe' => ['html']]);
+				return new TwigFunction($name, $name, ['is_safe' => ['html']]);
 			}
 			return false;
 		});
 
-		$twig->display(str_replace($this->config['view_path'], '', $template), $data);
+		$twig->display(basename($template), $data);
 	}
 
 	/**
 	 * 渲染模板内容
 	 * @access public
-	 * @param  string $template 模板内容
-	 * @param  array $data 模板变量
-	 * @param  array $config 模板参数
+	 * @param string $template 模板内容
+	 * @param array $data 模板变量
+	 * @param array $config 模板参数
 	 * @return void
-	 * @throws \Twig_Error_Loader
-	 * @throws \Twig_Error_Runtime
-	 * @throws \Twig_Error_Syntax
+	 * @throws Twig_Error_Loader
+	 * @throws Twig_Error_Runtime
+	 * @throws Twig_Error_Syntax
 	 */
 	public function display($template, $data = [], $config = [])
 	{
@@ -140,8 +161,8 @@ class Twig
 	/**
 	 * 自动定位模板文件
 	 * @access private
-	 * @param  string $template 模板文件规则
-	 * @return array
+	 * @param string $template 模板文件规则
+	 * @return string
 	 */
 	private function parseTemplate(string $template): string
 	{
@@ -168,7 +189,7 @@ class Twig
 			$controller = Loader::parseName($request->controller());
 
 			if ($controller) {
-				if ('' == $template) {
+				if ('' === $template) {
 					// 如果模板文件名为空 按照默认规则定位
 					$template = str_replace('.', DIRECTORY_SEPARATOR, $controller) . $depr . $this->getActionTemplate($request);
 				} elseif (false === strpos($template, $depr)) {
@@ -187,14 +208,13 @@ class Twig
 		$rule = [$request->action(true), Loader::parseName($request->action(true)), $request->action()];
 		$type = $this->config['auto_rule'];
 
-		return isset($rule[$type]) ? $rule[$type] : $rule[0];
+		return $rule[$type] ?? $rule[0];
 	}
 
 	/**
 	 * 获取模板引擎参数
 	 * @access private
-	 * @param  string|array $name 参数名
-	 * @param  mixed $value 参数值
+	 * @param string|array $name 参数名
 	 * @return mixed
 	 */
 	public function getConfig(string $name)
